5385f9a4be98600efe660cef6415f93e7c48b993
Mina Naguib <132841396+minanagib112@users.noreply.github.com>
2024-02-21 11:08:28 +0200

    updating sidebar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,6 +1,6 @@
 {{-- sidebar design --}}
 <div class="offcanvas offcanvas-start show" tabindex="-4" id="offcanvas" aria-labelledby="offcanvasLabel">
-    <div class="offcanvas-body d-flex flex-column p-4">
+    <div class="offcanvas-body d-flex flex-column p-2">
         <!-- Navigation tabs -->
         <!--Instagram logo-->
         <div class="mb-5">
@@ -118,6 +118,51 @@
             <img class="profile-img" src="{{}}" alt="">
             <a class="text-decoration-none text-dark" href="">Profile</a>
         </div>
+        <!-- Settings Tab -->
+
+        <div class="d-flex gap-3 align-items-center px-2 py-3 mb-3 nav-tab nav-tab-settings" id="settings-toggle">
+            <svg aria-label="Settings" class="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24"
+                role="img" viewBox="0 0 24 24" width="24">
+                <title>Settings</title>
+                <line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                    stroke-width="2" x1="3" x2="21" y1="4" y2="4"></line>
+                <line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                    stroke-width="2" x1="3" x2="21" y1="12" y2="12"></line>
+                <line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                    stroke-width="2" x1="3" x2="21" y1="20" y2="20"></line>
+            </svg>
+            <a class="text-decoration-none text-dark" href="#">Settings</a>
+        </div>
+        <div id="settings-content" class="settings-content" style="display: none;">
+            <ul class="list-unstyled d-flex flex-column">
+                <li class="settings-list">
+                    <svg aria-label="Settings" class="me-2" fill="currentColor" height="18" role="img"
+                        viewBox="0 0 24 24" width="18">
+                        <title>Settings</title>
+                        <circle cx="12" cy="12" fill="none" r="8.635" stroke="currentColor"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></circle>
+                        <path
+                            d="M14.232 3.656a1.269 1.269 0 0 1-.796-.66L12.93 2h-1.86l-.505.996a1.269 1.269 0 0 1-.796.66m-.001 16.688a1.269 1.269 0 0 1 .796.66l.505.996h1.862l.505-.996a1.269 1.269 0 0 1 .796-.66M3.656 9.768a1.269 1.269 0 0 1-.66.796L2 11.07v1.862l.996.505a1.269 1.269 0 0 1 .66.796m16.688-.001a1.269 1.269 0 0 1 .66-.796L22 12.93v-1.86l-.996-.505a1.269 1.269 0 0 1-.66-.796M7.678 4.522a1.269 1.269 0 0 1-1.03.096l-1.06-.348L4.27 5.587l.348 1.062a1.269 1.269 0 0 1-.096 1.03m11.8 11.799a1.269 1.269 0 0 1 1.03-.096l1.06.348 1.318-1.317-.348-1.062a1.269 1.269 0 0 1 .096-1.03m-14.956.001a1.269 1.269 0 0 1 .096 1.03l-.348 1.06 1.317 1.318 1.062-.348a1.269 1.269 0 0 1 1.03.096m11.799-11.8a1.269 1.269 0 0 1-.096-1.03l.348-1.06-1.317-1.318-1.062.348a1.269 1.269 0 0 1-1.03-.096"
+                            fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></path>
+                    </svg>
+                    <a class="text-decoration-none text-dark" href="#">Settings</a>
+                </li>
+                <li class="settings-list mb-3">
+                    <svg aria-label="Saved" class="me-2" fill="currentColor" height="18" role="img"
+                        viewBox="0 0 24 24" width="18">
+                        <title>Saved</title>
+                        <polygon fill="none" points="20 21 12 13.44 4 21 4 3 20 3 20 21" stroke="currentColor"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polygon>
+                    </svg>
+                    <a class="text-decoration-none text-dark" href="#">Saved
+                        Posts</a>
+                </li>
+                <li class="settings-list settings-list-logout"><a class="text-decoration-none text-dark"
+                        href="#">Logout</a></li>
+            </ul>
+        </div>
+
+
         <!-- Additional Tab Content -->
         <div class="d-none">
             <div class="searchTabContent">
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -17,7 +17,7 @@
     @include('includes.navbar')
     @yield('content')
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.js"></script>
